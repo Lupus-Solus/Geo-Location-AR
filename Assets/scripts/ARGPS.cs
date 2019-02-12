2b3a15e871d9bb1bc937f0c2bc0cd10c156d0987
Lupus Solus <lupus@lupussolus.com>
2019-02-12 18:27:06 +0000

    Initial Commit

---
+++ Assets/scripts/ARGPS.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ARGPS : MonoBehaviour
+{
+    public static ARGPS Instance { get; set; }
+
+    public bool Initialised = false;
+
+    public Text TextDevicePosition;
+    public Text TextInitialPosition;
+    public Text TextOrientation;
+    public Text TextARCameraPosition;
+
+    public Vector3 InitialDevicePosition = Vector3.zero;
+    public float Orientation;
+    public Camera ARCamera;
+
+    [HideInInspector]
+    public float Initial_Lat;
+    [HideInInspector]
+    public float Initial_Lon;
+    [HideInInspector]
+    public float Initial_Alt;
+
+    [HideInInspector]
+    public float Current_Lat;
+    [HideInInspector]
+    public float Current_Lon;
+    [HideInInspector]
+    public float Current_Alt;
+
+    float lowestCompass, highestCompass;
+
+
+    public float StabliseTime = 4;
+
+    bool initialising = true;
+    bool stabiliseStarted = false;
+    
+
+    void Start()
+    {
+        Instance = this;
+        Input.location.Start();
+        Input.compass.enabled = true;
+    }
+
+    void Update()
+    {
+        if(Input.location.status == LocationServiceStatus.Initializing)
+        {
+            TextDevicePosition.text = "Waiting for service to start.";
+        }
+
+        if (!stabiliseStarted && Input.location.status == LocationServiceStatus.Running)
+        {
+
+            TextDevicePosition.text = "Stablising...";
+            TextInitialPosition.text = "Stablising...";
+            StartCoroutine(InitialiseGPSCoords());
+            stabiliseStarted = true;
+
+        }
+
+        if (Input.location.status == LocationServiceStatus.Running)
+        {
+            SetGPSCoords();
+            
+        }
+    }
+
+    private IEnumerator InitialiseGPSCoords()
+    {
+        yield return new WaitForSeconds(StabliseTime);
+        initialising = false;
+    }
+
+    void SetGPSCoords()
+    {
+
+        if (Input.location.status == LocationServiceStatus.Initializing)
+        {
+            TextDevicePosition.text = "Location Service is Initialising";
+        }
+
+        if (Input.location.status == LocationServiceStatus.Running)
+        {
+            Current_Lat = Input.location.lastData.latitude;
+            Current_Lon = Input.location.lastData.longitude;
+            Current_Alt = Input.location.lastData.altitude;
+
+            if (!initialising)
+            {
+                if (!Initialised)
+                {
+                    Initial_Lat = Input.location.lastData.latitude;
+                    Initial_Lon = Input.location.lastData.longitude;
+                    Initial_Alt = Input.location.lastData.altitude;
+                                        
+                    InitialDevicePosition = new Vector3(Initial_Lat, 0, Initial_Lon);
+                   
+
+                    TextInitialPosition.text = "Saved pos lat: " + Initial_Lat + " lon: " + Initial_Lon + " alt: " + Initial_Alt;
+
+                    Orientation = Input.compass.magneticHeading;
+                    ARCamera.transform.rotation = Quaternion.Euler(0, Orientation, 0);
+
+                    lowestCompass = Input.compass.magneticHeading;
+                    highestCompass = Orientation = Input.compass.magneticHeading;
+
+                    Initialised = true;
+                }
+            }
+
+            var tempOrientation = Input.compass.magneticHeading;
+
+            if(tempOrientation < lowestCompass)
+            {
+                lowestCompass = tempOrientation;
+            }
+
+            if(tempOrientation > highestCompass)
+            {
+                highestCompass = tempOrientation;
+            }
+
+            TextOrientation.text = "Compass: Low:" + lowestCompass + "High: " + highestCompass + " Current:" + tempOrientation.ToString();
+            TextDevicePosition.text = "Current pos lat: " + Current_Lat + " lon: " + Current_Lon + " alt: " + Current_Alt;
+            TextARCameraPosition.text = string.Format("Cam Pos: {0} ", ARCamera.transform.position.ToString());
+        }
+
+    }
+
+
+
+
+}
---
+++ Assets/scripts/ARObject.cs
@@ -0,0 +1,82 @@
+﻿using OpenTK;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ARObject : MonoBehaviour
+{
+    public Vector3 ObjectGPSCoords;
+
+    public Text TextARPosition;
+
+    private float degreesLatitudeInMeters = 111132;
+    private float degreesLongitudeInMetersAtEquator = 111319.9f;
+
+    private bool transformSet = false;
+
+    private ARGPS arGPS;
+
+    private Vector3d arObjectPosition;
+
+    void Start()
+    {
+        arGPS = ARGPS.Instance;
+
+        arObjectPosition = new Vector3d(ObjectGPSCoords);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(arGPS == null)
+        {
+            arGPS = ARGPS.Instance;
+        }
+
+        if(arGPS == null)
+        {
+            return;
+        }
+
+        //TextARPosition.text = "arGPS initialised: " + arGPS.Initialised;
+        if (arGPS.Initialised)
+        {
+            
+            PlaceObjectInARWorld();
+        }
+    }
+
+    private double GetLongitudeDegreeDistance(double latitude)
+    {
+        return degreesLongitudeInMetersAtEquator * Math.Cos(latitude * (Math.PI / 180));
+    }
+
+    void PlaceObjectInARWorld()
+    {
+        if (!transformSet)
+        {
+            arGPS.ARCamera.transform.rotation = Quaternion.Euler(0, arGPS.Orientation, 0);
+
+            var deviceGpsPosition = new Vector3d(arGPS.InitialDevicePosition);
+            
+            var offset = (arObjectPosition - deviceGpsPosition) * new Vector3d(degreesLatitudeInMeters, 1, GetLongitudeDegreeDistance(deviceGpsPosition.X));
+            var heading = MathHelper.DegreesToRadians(arGPS.Orientation);
+
+            var t = Quaterniond.FromEulerAngles(0, -heading, 0);
+
+            var rotatedOffset = t * offset;
+
+            var floatPosition = new Vector3((float)rotatedOffset.X, (float)rotatedOffset.Y, (float)rotatedOffset.Z);
+            transform.position = new Vector3(floatPosition.x, 0, floatPosition.z);
+            transform.localScale = new Vector3(1, 1, 1);
+
+            if(TextARPosition != null)
+            {
+                TextARPosition.text = "AR Object placed at: x: " + transform.position.x + ", y: 0, z: " + transform.position.z;
+            }
+
+            transformSet = true;
+        }
+
+    }
+}
